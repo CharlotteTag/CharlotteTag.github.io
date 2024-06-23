2eafa3e047320b2df274f9134d01fdbecba0dc38
CharlotteUbuntu <ckalutycz@gmail.com>
2024-06-23 17:47:18 -0600

    Add js file

---
+++ static/populate_page.js
@@ -0,0 +1,8 @@
+'use strict';
+
+window.addEventListener('load', (event) => {
+    //const header = document.createElement("header");
+    const title = document.createElement("h1");
+    title.textContent = "Charlotte Kalutycz";
+    //const navigation = document.createElement("nav");
+})
