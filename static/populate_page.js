13c77e661e925cb4420ac99940c8f303d88184b3
CharlotteUbuntu <ckalutycz@gmail.com>
2024-06-23 18:03:04 -0600

    Work with html pages

--- static/populate_page.js
+++ static/populate_page.js
@@ -1,8 +1,8 @@
 'use strict';
 
 window.addEventListener('load', (event) => {
-    //const header = document.createElement("header");
+    const header = document.getElementById('header');
     const title = document.createElement("h1");
     title.textContent = "Charlotte Kalutycz";
-    //const navigation = document.createElement("nav");
+    header.append(title);
 })
