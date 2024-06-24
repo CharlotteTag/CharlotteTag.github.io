90e25333f4e71543a3395401e4e29009de07570f
CharlotteUbuntu <ckalutycz@gmail.com>
2024-06-23 19:00:37 -0600

    Edit header

--- static/populate_page.js
+++ static/populate_page.js
@@ -12,21 +12,26 @@ function createHeader() {
     // Create the title and append it to the header
     const title = document.createElement("a");
     title.id = "title"
-    title.textContent = "Charlotte Kalutycz";
     title.href = "/index.html"
+    const image = document.createElement("img");
+    image.src = "/images/title.png"
+    image.alt = "Charlotte Kalutycz"
+    title.append(image);
     header.append(title);
 
     // Create the navigation menu and append it to the header
     const navigation = document.createElement("nav");
     const u_list = document.createElement("ul");
-
-    const edu_li = document.createElement("li");
-    const edu_a = document.createElement("a");
-    edu_a.textContent = "Education";
-    edu_a.href = "/content/education.html"
-    edu_li.append(edu_a);
-    u_list.append(edu_li);
-
+    nav_options = ["Education", "Work Experience"]
+    nav_links = ["/content/education.html", "/content/work.html"]
+    for (let i=0; i < nav_options.length; i++) {
+        const current_li = document.createElement("li");
+        const current_a = document.createElement("a");
+        current_a.textContent = nav_options[i];
+        current_a.href = nav_links[i];
+        current_li.append(current_a);
+        u_list.append(current_li);
+    }
     navigation.append(u_list);
     header.append(navigation);
 }
@@ -34,3 +39,4 @@ function createHeader() {
 function createFooter() {
     const footer = document.getElementById('footer');
 }
+
