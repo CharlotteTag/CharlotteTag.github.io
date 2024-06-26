9f88464c2f9529c033f181d540c86466c3a6c53f
CharlotteUbuntu <ckalutycz@gmail.com>
2024-06-25 19:38:37 -0600

    Create footer

--- static/populate_page.js
+++ static/populate_page.js
@@ -3,7 +3,7 @@
 window.addEventListener('load', (event) => {
     // Once the page has loaded, add the consistent page elements
     createHeader();
-    //createFooter();
+    createFooter();
 })
 
 function createHeader() {
@@ -11,16 +11,16 @@ function createHeader() {
 
     // Create the title and append it to the header
     const title = document.createElement("a");
-    title.id = "title"
-    title.href = "/index.html"
-    title.textContent = "Charlotte Kalutycz"
+    title.id = "title";
+    title.href = "/index.html";
+    title.textContent = "Charlotte Kalutycz";
     header.append(title);
 
     // Create the navigation menu and append it to the header
     const navigation = document.createElement("nav");
     const u_list = document.createElement("ul");
-    let nav_options = ["Education", "Work Experience"]
-    let nav_links = ["/content/education.html", "/content/work.html"]
+    let nav_options = ["Education", "Work Experience"];
+    let nav_links = ["/content/education.html", "/content/work.html"];
     for (let i=0; i < nav_options.length; i++) {
         const current_li = document.createElement("li");
         const current_a = document.createElement("a");
@@ -35,5 +35,8 @@ function createHeader() {
 
 function createFooter() {
     const footer = document.getElementById('footer');
+    const summary = document.createElement("p");
+    summary.textContent = "Thank you for visiting!"
+    footer.append(summary);
 }
 
