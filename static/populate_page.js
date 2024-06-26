60e26d387d2169a0470ceffcad6eef624f7162fd
CharlotteUbuntu <ckalutycz@gmail.com>
2024-06-26 16:14:32 -0600

    Add footer nav menu + add more pages

--- static/populate_page.js
+++ static/populate_page.js
@@ -19,8 +19,8 @@ function createHeader() {
     // Create the navigation menu and append it to the header
     const navigation = document.createElement("nav");
     const u_list = document.createElement("ul");
-    let nav_options = ["Education", "Work Experience"];
-    let nav_links = ["/content/education.html", "/content/work.html"];
+    let nav_options = ["Education", "Work Experience", "Project Experience", "Extracurriculars"];
+    let nav_links = ["/content/education.html", "/content/workexperience.html", "/content/projectexperience.html", "/content/extracurriculars.html"];
     for (let i=0; i < nav_options.length; i++) {
         const current_li = document.createElement("li");
         const current_a = document.createElement("a");
@@ -38,5 +38,17 @@ function createFooter() {
     const summary = document.createElement("p");
     summary.textContent = "Thank you for visiting!"
     footer.append(summary);
+    let nav_options = ["Education", "Work Experience", "Project Experience", "Extracurriculars"];
+    let nav_links = ["/content/education.html", "/content/workexperience.html", "/content/projectexperience.html", "/content/extracurriculars.html"];
+    for (let i=0; i < nav_options.length; i++) {
+        const current_li = document.createElement("li");
+        const current_a = document.createElement("a");
+        current_a.textContent = nav_options[i];
+        current_a.href = nav_links[i];
+        current_li.append(current_a);
+        u_list.append(current_li);
+    }
+    navigation.append(u_list);
+    header.append(navigation);
 }
 
