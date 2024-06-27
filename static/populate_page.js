0aa23d1e747f405fa1d0c15ba66e460f71ca3993
CharlotteUbuntu <ckalutycz@gmail.com>
2024-06-27 16:16:34 -0600

    Add contact menu to footer

--- static/populate_page.js
+++ static/populate_page.js
@@ -19,11 +19,11 @@ function createHeader() {
     // Create the navigation menu and append it to the header
     const navigation = document.createElement("nav");
     const u_list = document.createElement("ul");
-    let nav_options = ["Education", "Work Experience", "Project Experience", "Extracurriculars"];
-    let nav_links = ["/content/education.html", "/content/workexperience.html", "/content/projectexperience.html", "/content/extracurriculars.html"];
+    const nav_options = ["Education", "Work Experience", "Project Experience", "Extracurriculars"];
+    const nav_links = ["/content/education.html", "/content/workexperience.html", "/content/projectexperience.html", "/content/extracurriculars.html"];
     for (let i=0; i < nav_options.length; i++) {
-        const current_li = document.createElement("li");
-        const current_a = document.createElement("a");
+        let current_li = document.createElement("li");
+        let current_a = document.createElement("a");
         current_a.textContent = nav_options[i];
         current_a.href = nav_links[i];
         current_li.append(current_a);
@@ -39,11 +39,11 @@ function createFooter() {
     // Create the navigation menu ahnd append it to the footer
     const navigation = document.createElement("nav");
     const u_list = document.createElement("ul");
-    let nav_options = ["Home", "Education", "Work Experience", "Project Experience", "Extracurriculars"];
-    let nav_links = ["/index.html", "/content/education.html", "/content/workexperience.html", "/content/projectexperience.html", "/content/extracurriculars.html"];
+    const nav_options = ["Home", "Education", "Work Experience", "Project Experience", "Extracurriculars"];
+    const nav_links = ["/index.html", "/content/education.html", "/content/workexperience.html", "/content/projectexperience.html", "/content/extracurriculars.html"];
     for (let i=0; i < nav_options.length; i++) {
-        const current_li = document.createElement("li");
-        const current_a = document.createElement("a");
+        let current_li = document.createElement("li");
+        let current_a = document.createElement("a");
         current_a.textContent = nav_options[i];
         current_a.href = nav_links[i];
         current_li.append(current_a);
@@ -51,5 +51,24 @@ function createFooter() {
     }
     navigation.append(u_list);
     footer.append(navigation);
+
+    // Create the contact menu in the footer
+    const contact = document.createElement("h1");
+    contact.textContent = "Connect with me!"
+    footer.append(contact)
+    const contact_nav = document.createElement("nav");
+    const contact_list = document.createElement("ul");
+    const contact_options = ["LinkedIn", "GitHub"];
+    const contact_links = ["https://www.linkedin.com/in/charlotte-kalutycz-5459572aa/", "https://github.com/CharlotteTag"];
+    for (let i=0; i < nav_options.length; i++) {
+        let current_li = document.createElement("li");
+        let current_a = document.createElement("a");
+        current_a.textContent = contact_options[i];
+        current_a.href = contact_links[i];
+        current_li.append(current_a);
+        contact_list.append(current_li);
+    }
+    contact_nav.append(contact_list);
+    footer.append(contact_nav);
 }
 
